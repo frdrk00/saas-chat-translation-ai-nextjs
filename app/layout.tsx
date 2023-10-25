6b0b5c1000b17577459ae7b74b9792c069fb7dba
frdrk00 <masterkooo@gmail.com>
2023-10-25 17:46:41 +0200

    HomePage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/components/header'
 
 import { ThemeProvider } from '@/components/theme-provider'
 import { ClientProviders } from '@/components/providers/client-providers'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'SaaS Translation Chat',
@@ -21,7 +18,7 @@ export default function RootLayout({
   return (
     <ClientProviders>
       <html lang="en">
-        <body className={inter.className}>
+        <body className="flex flex-col min-h-screen">
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
