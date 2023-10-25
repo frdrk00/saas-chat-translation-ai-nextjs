96b9c90b9e7313bd30c1351b5fcdeca5eb60e584
frdrk00 <masterkooo@gmail.com>
2023-10-25 16:18:24 +0200

    Header 2/2

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,15 @@
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/auth'
+
 import { DarkModeToggle } from '@/components/dark-mode-toggle'
 import { Logo } from '@/components/logo'
 import { UserButton } from '@/components/user-button'
+import Link from 'next/link'
+import { MessagesSquareIcon } from 'lucide-react'
 
-export const Header = () => {
+export const Header = async () => {
+  const session = await getServerSession(authOptions)
+
   return (
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
       <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
@@ -11,7 +18,15 @@ export const Header = () => {
         <div className="flex-1 flex items-center justify-end space-x-4">
           {/* Language Select */}
 
-          {/* Session & */}
+          {session ? (
+            <>
+              <Link href="/chat" prefetch={false}>
+                <MessagesSquareIcon className="text-black dark:text-white" />
+              </Link>
+            </>
+          ) : (
+            <Link href="/pricing">Pricing</Link>
+          )}
 
           <DarkModeToggle />
 
