e0afeb1adea11f3f73282c2657e6356454e3edfb
frdrk00 <masterkooo@gmail.com>
2023-10-25 22:15:42 +0200

    Checkout Button Functionality

--- components/checkout-button.tsx
+++ components/checkout-button.tsx
@@ -1,14 +1,49 @@
 'use client'
 
+import { db } from '@/firebase'
+import { addDoc, collection, onSnapshot } from 'firebase/firestore'
 import { useSession } from 'next-auth/react'
+import { useState } from 'react'
 
 export const CheckoutButton = () => {
   const { data: session } = useSession()
+  const [loading, setLoading] = useState(false)
+
   const createCheckoutSession = async () => {
-    if (!session) return
+    if (!session?.user.id) return
 
     //push into firestore
+    setLoading(true)
+
+    const docRef = await addDoc(
+      collection(db, 'customers', session.user.id, 'checkout_sessions'),
+      {
+        price: 'price_1O5D3zLybJkapqHX9lDy4cZd',
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      }
+    )
+
     //stripe ext login on firebase
+    return onSnapshot(docRef, (snap) => {
+      const data = snap.data()
+      const url = data?.url
+      const error = data?.error
+
+      if (error) {
+        // Show an  error to your customer and
+        // inspect your Cloud Function logs in the Firebase console.
+        alert(`An error occured: ${error.message}`)
+        setLoading(false)
+      }
+
+      if (url) {
+        // We have a Stripe Checkout URL, let's redirect.
+        window.location.assign(url)
+        setLoading(false)
+      }
+    })
+
     //redirect to checkout page
   }
   return (
@@ -17,7 +52,7 @@ export const CheckoutButton = () => {
         onClick={() => createCheckoutSession()}
         className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default"
       >
-        Sign Up
+        {loading ? 'loading...' : 'Sign Up'}
       </button>
     </div>
   )
--- constants/index.ts
+++ constants/index.ts
@@ -15,7 +15,7 @@ export const tiers = [
   },
   {
     name: 'Pro',
-    id: 'si_OnlcsLNQYbMVzV',
+    id: 'pro',
     href: '#',
     priceMonthly: '$5.99',
     description: 'Unlock the Full Potential with Pro!',
