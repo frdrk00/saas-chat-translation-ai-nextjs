e4a9ffd53f1454b8272d09e6129bfc6fb060eec6
frdrk00 <masterkooo@gmail.com>
2023-10-26 16:37:30 +0200

    Chat List & Chat List Row(s)

---
+++ components/chat-list-row.tsx
@@ -0,0 +1,3 @@
+export const ChatListRow = () => {
+  return <div></div>
+}
--- components/chat-list.tsx
+++ components/chat-list.tsx
@@ -1,12 +1,21 @@
-import { authOptions } from '@/auth'
-import { getDocs } from 'firebase/firestore'
 import { getServerSession } from 'next-auth'
+import { getDocs } from 'firebase/firestore'
+
+import { authOptions } from '@/auth'
+import { chatMembersCollectionGroupRef } from '@/lib/converters/chat-members'
+
+import { ChatListRows } from '@/components/chat-list-rows'
 
 export const ChatList = async () => {
   const session = await getServerSession(authOptions)
 
-  //   const chatSnapshot = await getDocs(
-  //     chatMembersCollectionGroupRef(session?.user.id!)
-  //   )
-  return <div></div>
+  const chatsSnapshot = await getDocs(
+    chatMembersCollectionGroupRef(session?.user.id!)
+  )
+
+  const initialChats = chatsSnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    timestamp: null,
+  }))
+  return <ChatListRows initialChats={initialChats} />
 }
